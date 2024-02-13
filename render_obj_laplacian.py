e5b084860bd1ac8db8a0ed8c592a26e99ee0570c
chacorp <chacorp@kaist.ac.kr>
2024-02-13 14:49:51 +0900

    clean up - laplacian practice code

--- render_obj_laplacian.py
+++ render_obj_laplacian.py
@@ -2,178 +2,34 @@ from __future__ import print_function
 
 import os
 from os.path import exists, join, split
-from glob import glob
+
+import sys
+import json
+import torch
 import numpy as np
+import pickle as pkl
+from glob import glob
+from tqdm import tqdm
+from PIL import Image
+from easydict import EasyDict
+
+# from scipy.sparse.linalg import cg
+import scipy.sparse
 
 import glfw
 from OpenGL.GL import *
 import OpenGL.GL.shaders as shaders
-import numpy as np
-from PIL import Image
 import glm
-from easydict import EasyDict
 
 # from testwindow import *
 from imgui.integrations.glfw import GlfwRenderer
 import imgui
-import sys
+
+# from pytorch3d.structures import Meshes
 from pytorch3d.io import load_obj, save_obj, load_objs_as_meshes
-import json
-import torch
-import pickle as pkl
-from tqdm import tqdm
-
-from pytorch3d.structures import Meshes
 # from pytorch3d.ops import laplacian_matrix
-from scipy.sparse.linalg import cg
-import scipy.sparse
-import numpy as np
 
-# ------------------------ Laplacian Matrices ------------------------ #
-# This file contains implementations of differentiable laplacian matrices.
-# These include
-# 1) Standard Laplacian matrix
-# 2) Cotangent Laplacian matrix
-# 3) Norm Laplacian matrix
-# -------------------------------------------------------------------- #
-
-def adjacency_matrix(verts: torch.Tensor, edges: torch.Tensor):
-    V = verts.shape[0]
-
-    e0, e1 = edges.unbind(1)
-
-    idx01 = torch.stack([e0, e1], dim=1)  # (E, 2)
-    idx10 = torch.stack([e1, e0], dim=1)  # (E, 2)
-    idx = torch.cat([idx01, idx10], dim=0).t()  # (2, 2*E)
-
-    # First, we construct the adjacency matrix,
-    # i.e. A[i, j] = 1 if (i,j) is an edge, or
-    # A[e0, e1] = 1 &  A[e1, e0] = 1
-    ones = torch.ones(idx.shape[1], dtype=torch.float32, device=verts.device)
-    A = torch.sparse.FloatTensor(idx, ones, (V, V))
-    return A
-
-def laplacian_matrix(verts: torch.Tensor, edges: torch.Tensor):
-    """
-    Computes the laplacian matrix.
-    The definition of the laplacian is
-    L[i, j] =    -1       , if i == j
-    L[i, j] = 1 / deg(i)  , if (i, j) is an edge
-    L[i, j] =    0        , otherwise
-    where deg(i) is the degree of the i-th vertex in the graph.
-
-    Args:
-        verts: tensor of shape (V, 3) containing the vertices of the graph
-        edges: tensor of shape (E, 2) containing the vertex indices of each edge
-    Returns:
-        L: Sparse FloatTensor of shape (V, V)
-    """
-    V = verts.shape[0]
-
-    e0, e1 = edges.unbind(1)
-
-    idx01 = torch.stack([e0, e1], dim=1)  # (E, 2)
-    idx10 = torch.stack([e1, e0], dim=1)  # (E, 2)
-    idx = torch.cat([idx01, idx10], dim=0).t()  # (2, 2*E)
-
-    # First, we construct the adjacency matrix,
-    # i.e. A[i, j] = 1 if (i,j) is an edge, or
-    # A[e0, e1] = 1 &  A[e1, e0] = 1
-    ones = torch.ones(idx.shape[1], dtype=torch.float32, device=verts.device)
-    A = torch.sparse.FloatTensor(idx, ones, (V, V))
-
-    # the sum of i-th row of A gives the degree of the i-th vertex
-    deg = torch.sparse.sum(A, dim=1).to_dense()
-
-    # We construct the Laplacian matrix by adding the non diagonal values
-    # i.e. L[i, j] = 1 ./ deg(i) if (i, j) is an edge
-    deg0 = deg[e0]
-    # pyre-fixme[58]: `/` is not supported for operand types `float` and `Tensor`.
-    deg0 = torch.where(deg0 > 0.0, 1.0 / deg0, deg0)
-    deg1 = deg[e1]
-    # pyre-fixme[58]: `/` is not supported for operand types `float` and `Tensor`.
-    deg1 = torch.where(deg1 > 0.0, 1.0 / deg1, deg1)
-    val = torch.cat([deg0, deg1])
-    L = torch.sparse.FloatTensor(idx, val, (V, V))
-
-    # Then we add the diagonal values L[i, i] = -1.
-    idx = torch.arange(V, device=verts.device)
-    idx = torch.stack([idx, idx], dim=0)
-    ones = torch.ones(idx.shape[1], dtype=torch.float32, device=verts.device)
-    L -= torch.sparse.FloatTensor(idx, ones, (V, V))
-
-    return L
-
-def compute_face_norm(vn, f):
-    v1 = vn[f:, 0]
-    v2 = vn[f:, 1]
-    v3 = vn[f:, 2]
-    e1 = v1 - v2
-    e2 = v2 - v3
-
-    return np.cross(e1, e2)
-
-def LoadTexture(filename):
-    pBitmap = Image.open(filename)
-    pBitmap = pBitmap.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
-    glformat = GL_RGB # if pBitmap.mode == "RGB" else GL_RGBA
-    pBitmap = pBitmap.convert('RGB') # 'RGBA
-    pBitmapData = np.array(pBitmap, np.uint8)
-    
-    # pBitmapData = np.array(list(pBitmap.getdata()), np.int8)
-    texName = glGenTextures(1)
-    # import pdb; pdb.set_trace()    
-    glBindTexture(GL_TEXTURE_2D, texName)
-    glTexImage2D(
-        GL_TEXTURE_2D, 0, GL_RGB, pBitmap.size[0], pBitmap.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, pBitmapData
-    )
-
-    # glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
-    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    
-    ### Texture Wrapping
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    
-    ### Texture Filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    """        
-        GL_NEAREST_MIPMAP_NEAREST: nearest mipmap to match the pixel size and uses nearest neighbor interpolation for texture sampling.
-        GL_LINEAR_MIPMAP_NEAREST:  nearest mipmap level and samples that level using linear interpolation.
-        GL_NEAREST_MIPMAP_LINEAR:  linearly interpolates between the two closest mipmaps & samples via nearest neighbor interpolation.
-        GL_LINEAR_MIPMAP_LINEAR:   linearly interpolates between the two closest mipmaps & samples via linear interpolation.
-    """
-    
-    # glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-    glGenerateMipmap(GL_TEXTURE_2D)
-    return texName
-
-def load_textures(filenames):
-    texture = glGenTextures(len(filenames))
-    for i, filename in enumerate(filenames):
-        pBitmap = Image.open(filename)
-        pBitmap = pBitmap.transpose(Image.Transpose.FLIP_TOP_BOTTOM) 
-        # glformat = GL_RGB # if pBitmap.mode == "RGB" else GL_RGBA
-        pBitmap = pBitmap.convert('RGB') # 'RGBA
-        pBitmapData = np.array(pBitmap, np.uint8)
-            
-    
-        glBindTexture(GL_TEXTURE_2D, texture[i])
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-        glTexImage2D(
-            GL_TEXTURE_2D, 0, GL_RGB, pBitmap.size[0], pBitmap.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, pBitmapData
-        )
-    return texture
+from utils.util import *
 
 def load_texture(path):
     texture = glGenTextures(1)
@@ -186,58 +42,6 @@ def load_texture(path):
     glGenerateMipmap(GL_TEXTURE_2D)
     glBindTexture(GL_TEXTURE_2D, texture)
     return texture
-
-def rotation(M, angle, x, y, z):
-    angle = np.pi * angle / 180.0
-    c, s = np.cos(angle), np.sin(angle)
-    n = np.sqrt(x*x + y*y + z*z)
-    x,y,z = x/n, y/n, z/n
-    cx,cy,cz = (1-c)*x, (1-c)*y, (1-c)*z
-    R = np.array([[cx*x + c,   cy*x - z*s, cz*x + y*s, 0.0],
-                  [cx*y + z*s, cy*y + c,   cz*y - x*s, 0.0],
-                  [cx*z - y*s, cy*z + x*s, cz*z + c,   0.0],
-                  [0.0,        0.0,        0.0,        1.0]], dtype=M.dtype)
-
-    return np.dot(M, R.T)
-
-def y_rotation(angle):
-    angle = np.pi * angle / 180.0
-    c, s = np.cos(angle), np.sin(angle)
-    R = np.array([[   c,       0.0,          s,        0.0],
-                  [ 0.0,       1.0,        0.0,        0.0],
-                  [  -s,       0.0,          c,        0.0],
-                  [ 0.0,       0.0,        0.0,        1.0]], dtype=np.float32)
-    return R
-
-def z_rotation(angle):
-    angle = np.pi * angle / 180.0
-    c, s = np.cos(angle), np.sin(angle)
-    R = np.array([[   c,        -s,        0.0,        0.0],
-                  [   s,         c,        0.0,        0.0],
-                  [ 0.0,       0.0,        1.0,        0.0],
-                  [ 0.0,       0.0,        0.0,        1.0]], dtype=np.float32)
-    return R
-
-def x_rotation(angle):
-    angle = np.pi * angle / 180.0
-    c, s = np.cos(angle), np.sin(angle)
-    R = np.array([[ 1.0,       0.0,        0.0,        0.0],
-                  [ 0.0,         c,         -s,        0.0],
-                  [ 0.0,         s,          c,        0.0],
-                  [ 0.0,       0.0,        0.0,        1.0]], dtype=np.float32)
-    return R
-
-def normalize_v(V):
-    V = np.array(V)
-    V = (V-(V.max(0)+V.min(0))*0.5)/max(V.max(0)-V.min(0))
-    # V = (V-(V.max(0).values + V.min(0).values) * 0.5)/max(V.max(0).values - V.min(0).values)
-    
-    # FLAME
-    # V = V - V.mean(0).values
-    # V = V - V.min(0).values[1]
-    # V = V / V.max(0).values[1]
-    # V = (V * 2.0) - 1.0
-    return V
 
 def load_obj_mesh(mesh_path):
     mesh = EasyDict()
@@ -280,100 +84,6 @@ def load_obj_mesh(mesh_path):
     mesh.ft = np.array(face_texture) -1
     mesh.fn = np.array(face_normal) -1
     return mesh
-
-def vertex_normal(v1, v2, v3):
-    v1c = np.cross(v2 - v1, v3 - v1)
-    v1n = v1c/np.linalg.norm(v1c)
-    return v1n
-
-def computeTangentBasis(vertex, uv):
-    tangents = []
-    tangents = np.zeros_like(vertex)
-    # bitangents = []
-    for idx in range(0, len(vertex)//3):
-        
-        # import pdb;pdb.set_trace()
-        offset = idx*3
-        v0 = vertex[offset]
-        v1 = vertex[offset+1]
-        v2 = vertex[offset+2]
-
-        offset = idx*3
-        uv0 =    uv[offset]
-        uv1 =    uv[offset+1]
-        uv2 =    uv[offset+2]
-        #print v0,v1,v2
-        deltaPos1 = np.array([v1[0]-v0[0], v1[1]-v0[1], v1[2]-v0[2]])
-        deltaPos2 = np.array([v2[0]-v0[0], v2[1]-v0[1], v2[2]-v0[2]])
-
-        deltaUV1 = np.array([uv1[0]-uv0[0], uv1[1]-uv0[1]])
-        deltaUV2 = np.array([uv2[0]-uv0[0], uv2[1]-uv0[1]])
-
-        f = 1.0 / (deltaUV1[0] * deltaUV2[1] - deltaUV1[1] * deltaUV2[0])
-        tangent = (deltaPos1 * deltaUV2[1]   - deltaPos2 * deltaUV1[1]) * f
-        # bitangent = (deltaPos2 * deltaUV1[0]   - deltaPos1 * deltaUV2[0]) * f
-
-        tangents[offset]   = tangent
-        tangents[offset+1] = tangent
-        tangents[offset+2] = tangent
-    # import pdb;pdb.set_trace()
-    return tangents
-
-import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse.linalg import factorized
-
-class SolveLaplacian:
-    def __init__(self, mesh):
-        self.mesh = mesh
-        self.A = None
-        self.ATA = None
-        self.solver = None
-        self.ATb = None
-        self.x = [None, None, None]
-        self.b = [None, None, None]
-        self.build_system_matrix()
-
-        m, n = self.A.shape
-        self.ATb = np.zeros(n)
-        for i in range(3):
-            self.x[i] = np.zeros(n)
-            self.b[i] = np.zeros(m)
-            for j, v in enumerate(mesh.vList):
-                self.x[i][j] = v.Position()[i]
-            self.b[i] = self.A @ self.x[i]
-
-        self.build_system_matrix()
-        self.build_matrix_ATA()
-        self.solver = self.factorization()
-        print("ok" if self.solver else "fail")
-
-    def build_system_matrix(self):
-        raise NotImplementedError
-
-    def build_matrix_ATA(self):
-        AT = self.A.transpose()
-        self.ATA = AT @ self.A
-
-    def factorization(self):
-        print("Factorization")
-        ATA_csr = csr_matrix(self.ATA)
-        solver = factorized(ATA_csr)
-        return solver
-
-    def deform(self):
-        vList = self.mesh.Vertices()
-        n = len(vList)
-        for i in range(3):
-            for j, v in enumerate(vList):
-                if v.Flag():
-                    # add constraints with weight = 1000 
-                    self.b[i][j+n] = v.Position()[i] * 1000
-            self.ATb = self.A.T @ self.b[i]
-            self.x[i] = self.solver(self.ATb)
-        for j, v in enumerate(vList):
-            if v.Flag() == 0:
-                v.SetPosition(np.array([self.x[0][j], self.x[1][j], self.x[2][j]]))
 
 def render(resolution=512, mesh=None):
     if mesh is None:
